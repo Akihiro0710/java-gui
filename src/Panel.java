0f1e3d0677ea45a4fc92151dfa003290327cb1c2
akihiro0710 <acedia2074@gmail.com>
2018-04-17 21:47:45 +0900

    OvalShapeを使用した描画を実行

--- src/Panel.java
+++ src/Panel.java
@@ -1,10 +1,20 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by ta on 2018/04/17.
  */
-public class Panel extends JPanel{
-    public Panel(){
+public class Panel extends JPanel {
+    private Paintable paintable;
+
+    public Panel() {
         super();
+        paintable = new OvalShape(10, 10, 10);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        paintable.paint((Graphics2D) g);
     }
 }
