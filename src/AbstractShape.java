fcf6ca0a2a44ac43a83d1503befef899ea52d924
akihiro0710 <acedia2074@gmail.com>
2018-04-17 21:39:49 +0900

    描画する形の抽象クラスを定義

---
+++ src/AbstractShape.java
@@ -0,0 +1,51 @@
+import java.awt.*;
+
+/**
+ * Created by ta on 2018/04/17.
+ */
+public abstract class AbstractShape {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    public AbstractShape(int x, int y, int width, int height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public abstract void paint(Graphics2D g);
+}
