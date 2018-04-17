30dcdde8b8578714e7fd8e1b234bce647ecf12e0
akihiro0710 <acedia2074@gmail.com>
2018-04-17 20:41:41 +0900

    スクリーンのサイズ・表示位置を指定

--- src/Screen.java
+++ src/Screen.java
@@ -3,8 +3,10 @@ import javax.swing.*;
 /**
  * Created by ta on 2018/04/17.
  */
-public class Screen extends JFrame{
-    public Screen(String title){
+public class Screen extends JFrame {
+    public Screen(String title) {
         super(title);
+        setSize(400, 300);
+        setLocation(100, 100);
     }
 }
